7ebf228d9ee6502623a6b5fa31701d9eaacc12f8
InDev <InDevRus@hotmail.com>
2023-03-09 03:32:11 +0500

    Solved

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char symbolC = 99;
+        char symbolH = 104;
+        char symbolA = 97;
+        char symbolR = 114;
+
+        System.out.println(symbolC);
+        System.out.println(symbolH);
+        System.out.println(symbolA);
+        System.out.println(symbolR);
     }
 }
